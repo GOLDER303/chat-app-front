e204591582aa5f3856ab33f28ad3ddd20656a5b0
GOLDER303 <szymonstepien@interia.pl>
2023-06-08 12:17:23 +0200

    Add colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#202329",
+                secondary: "#2E343D",
+                dark: "#131313",
+                bright: "#8E8E97",
+                acent: "#6B8AFD",
+            },
+        },
     },
     plugins: [],
 }
