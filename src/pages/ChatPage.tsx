6e04e33468da04c7024c5fcedc23f2b980bae31a
GOLDER303 <szymonstepien@interia.pl>
2023-06-08 15:32:25 +0200

    Create chat page

---
+++ src/pages/ChatPage.tsx
@@ -0,0 +1,11 @@
+import ChatsPanel from "../components/chats/ChatsPanel"
+
+const ChatPage: React.FC = () => {
+    return (
+        <div className="min-h-screen bg-dark text-white">
+            <ChatsPanel />
+        </div>
+    )
+}
+
+export default ChatPage
