0dc6fd76c44a0fe746b13a34eca4ee09ce5f5215
GOLDER303 <szymonstepien@interia.pl>
2023-06-08 15:33:00 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import ChatsPanel from "./components/chats/ChatsPanel"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import ChatPage from "./pages/ChatPage"
+import LoginPage from "./pages/LoginPage"
+
+const router = createBrowserRouter([
+    {
+        path: "/chats/:id",
+        element: <ChatPage />,
+        children: [],
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+        children: [],
+    },
+])
 
 const App: React.FC = () => {
-    return (
-        <div className="min-h-screen bg-dark text-white">
-            <ChatsPanel />
-        </div>
-    )
+    return <RouterProvider router={router} />
 }
 
 export default App
