01cbe4f74bb8c4b9def65298428713fde335305e
GOLDER303 <szymonstepien@interia.pl>
2023-06-09 11:27:47 +0200

    Add refresh tokens function

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -22,3 +22,24 @@ export const login = async (email: string, password: string): Promise<void> => {
         throw new Error("Something went wrong")
     }
 }
+
+export const refreshTokens = async () => {
+    try {
+        const response = await axios.post("http://localhost:3000/auth/refresh", {
+            refreshToken: localStorage.getItem("jwt_refresh"),
+        })
+
+        localStorage.setItem("jwt", response.data.accessToken)
+        localStorage.setItem("jwt_refresh", response.data.refreshToken)
+    } catch (err) {
+        const error = err as AxiosError
+
+        const status = error.response?.status
+
+        if (status === 401) {
+            throw new UnauthorizedError()
+        }
+
+        throw new Error("Something went wrong")
+    }
+}
